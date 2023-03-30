40ee40ea24eddb5e516f1d5f93a06d8b6ed8d10a
StevenMo <stevenmolvin@gmail.com>
2023-03-30 14:09:33 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
 
-document.addEventListener('DOMContentLoaded', init);
+    console.log(input.value)
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      
+      .then((data) => {
+        const title = document.getElementById("movieTitle");
+        const summary = document.getElementById("movieSummary");
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
